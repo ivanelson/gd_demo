5224a4b42f47c3bbdf8455755094e045997e45a6
ivanelson_nunes <ivanelson_nunes@hotmail.com>
2015-03-07 22:59:45 -0300

    vou enviar p teste de merge e rebase

--- gcli.cmd
+++ gcli.cmd
@@ -4,6 +4,7 @@ find 2  cliente where cd_cliente = 1
 find 2  cliente where cd_cliente = 1
 find 2  cliente where cd_cliente = 1
 find 2  cliente where cd_cliente = 1
+find 1000  cliente where cd_cliente = 1
 
 transaction 
 endtransaction
